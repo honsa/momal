e5ff9643f4514949af2af916feb1d7f0cde6a6a5
honsa <straka@8038.ch>
2026-01-02 11:27:39 +0100

    refactor(game): tidy up Player model

--- src/Game/Player.php
+++ src/Game/Player.php
@@ -8,20 +8,31 @@ final class Player
 {
     public int $score = 0;
 
+    public readonly string $name;
+    public readonly string $roomId;
+
     public function __construct(
         public readonly string $connectionId,
-        public string $name,
-        public string $roomId
+        string $name,
+        string $roomId
     ) {
         $this->name = self::sanitizeName($name);
+        $this->roomId = $roomId;
+    }
+
+    public function withName(string $name): self
+    {
+        $p = new self($this->connectionId, $name, $this->roomId);
+        $p->score = $this->score;
+        return $p;
     }
 
     public static function sanitizeName(string $name): string
     {
         $name = \trim($name);
-        $name = \preg_replace('/\s+/', ' ', $name) ?? '';
+        $name = (string)(\preg_replace('/\s+/', ' ', $name) ?? '');
         $name = \mb_substr($name, 0, 20);
+
         return $name !== '' ? $name : 'Spieler';
     }
 }
-
